370f227e97209bf49317eafdaa21a8aef89187dc
DanteSlay <danteslay16@gmail.com>
2022-10-09 20:41:02 +0200

    Practica02 de Entornos de Desarrollo

--- Practicas/Practica02/ProyectoNetBeans/src/practica/HolaMundo.java
+++ Practicas/Practica02/ProyectoNetBeans/src/practica/HolaMundo.java
@@ -2,12 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Practica02;
+package practica;
 
 /**
  *
  * @author dante
  */
 public class HolaMundo {
-    
+    public static void main(String[] args){
+        System.out.println("¡Hola, Mundo!");
+    }
 }
---
+++ Practicas/Practica02/ProyectoNetBeans/src/proyectonetbeans/ProyectoNetBeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectonetbeans;
+
+/**
+ *
+ * @author dante
+ */
+public class ProyectoNetBeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
